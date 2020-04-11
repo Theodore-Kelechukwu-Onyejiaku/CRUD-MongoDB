757afe62ab63fbfeaa3ea439ee065bb51c2e1271
Theodore-Kelechukwu-Onyejiaku <theodoreonyejiaku@gmail.com>
2020-04-11 01:54:28 +0100

    modify codes

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,10 @@ const User = mongoose.model("User");
 
 
 //  GET REQUESTS
+
+
+
+//Index page
 router.get("/", (req, res)=>{
     User.find((err, doc)=>{
         if(err){
@@ -15,6 +19,19 @@ router.get("/", (req, res)=>{
     })
    
 })
+
+
+//Edit page
+router.get("/edit/:_id", (req, res)=>{
+    User.findById(req.params._id, (err, doc)=>{
+        if(err){
+            res.redirect("/")
+        }else{
+            res.render("edit", {message: "Edit, Ma/Sir", doc: doc})
+        }
+    })
+})
+
 
 //add oneself
 router.get("/add", (req, res)=>{
@@ -27,22 +44,16 @@ router.get("/add", (req, res)=>{
 router.get("/:_id", (req, res)=>{
     User.findById(req.params._id, (err, doc)=>{
         res.render("message", {message : doc})
-        console.log(doc)
     })
 })
 
-//Delete
-router.get("/delete/:_id", (req, res)=>{
-    User.findByIdAndDelete(req.params._id, (err, doc)=>{
-        if(err){
-            console.log(err);
-            res.render("index", {message: err})
-        }else{
-            res.redirect("/")
-        }
-        console.log(doc)
+//Delete question
+router.get("/item/:_id", (req, res)=>{
+    User.findById(req.params._id, (err, doc)=>{
+        res.render("delete", {item : doc})
     })
 })
+
 
 
 // POST REQUESTS
@@ -63,5 +74,42 @@ router.post("/add", (req, res)=>{
         }
     })
 })
+
+//Delete permanently
+router.post("/delete", (req, res)=>{
+    //console.log(req.body)
+    var code = req.body.code;
+    User.findById(req.body._id, (err, doc)=>{
+        if(!err){
+            if(code == doc.access_code){
+                User.findOneAndRemove(req.body._id, (err, doc)=>{
+                    if(err){
+                        console.log(err);
+                        res.render("index", {message: err})
+                    }else{
+                        res.redirect("/")
+                    }
+                })
+            }else{
+                res.render('delete', {message: "Access code is wrong, Lol, Hahahaha, have you forgotten?", item:doc})
+            }
+        }else{
+            res.render('delete', {message: "Wrong Access Code, Lol", item:doc})
+        }
+    })
+   
+})
+
+//Edit Post
+router.post("/edit", (req, res)=>{
+    User.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}, (err, doc)=>{
+        if(err){
+            res.render("edit", {message: "can't update"})
+        }else{
+            res.redirect("/")
+        }
+    })
+})
+
 
 module.exports = router;
